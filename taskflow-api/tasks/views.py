83bd06676a84871a3f10b1ad576f71b21bcb3005
MrYanis77 <yanislaldjipro@gmail.com>
2025-10-23 09:19:02 +0200

    Séance 2 – filtres, pagination, validations, owner, tests, fixtures

--- taskflow-api/tasks/views.py
+++ taskflow-api/tasks/views.py
@@ -1,10 +1,31 @@
+from rest_framework.exceptions import APIException
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
 
+class BusinessRuleException(APIException):
+    status_code = 400
+    default_detail = "Règle métier non respectée."
+
 class TaskViewSet(ModelViewSet):
+
+    # ... queryset, serializer_class, filters, etc.
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.status == "En cours":
+            raise BusinessRuleException("Impossible de supprimer une tâche 'En cours'.")
+        return super().destroy(request, *args, **kwargs)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({"message": "Tâche créée", "data":serializer.data}, status=status.HTTP_201_CREATED)
+
     queryset = Task.objects.all().order_by('-id')
     serializer_class = TaskSerializer
 
